37781bfe491e097c430bbafaf622c3c9289d599e
NEOpro-cyper <kaiskdishdisdi@gmail.com>
2026-03-17 19:35:57 +0530

    Update servers.controller.js

--- src/controllers/servers.controller.js
+++ src/controllers/servers.controller.js
@@ -1,10 +1,18 @@
-import { extractServers } from "../extractors/streamInfo.extractor.js";
+import { extractServers, extractMovieServers } from "../extractors/streamInfo.extractor.js";
 
 export const getServers = async (req) => {
   try {
+    // For TV episode: /api/servers/1676371
+    // For movie:      /api/servers/138514?type=movie
     const { id } = req.params;
+    const type = req.query.type || "tv";
+
     if (!id) throw new Error("Missing id param");
-    const servers = await extractServers(id);
+
+    const servers = type === "movie"
+      ? await extractMovieServers(id)
+      : await extractServers(id);
+
     return servers;
   } catch (e) {
     console.error(e);
