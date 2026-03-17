404a8f8eac1c4d100346048d17c5c4bb56f8915f
NEOpro-cyper <kaiskdishdisdi@gmail.com>
2026-03-18 03:23:02 +0530

    Update apiRoutes.js

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -1,12 +1,11 @@
 import * as homeInfoController from "../controllers/homeInfo.controller.js";
 import * as categoryController from "../controllers/category.controller.js";
-import * as animeInfoController from "../controllers/animeInfo.controller.js";
+import * as mediaInfoController from "../controllers/animeInfo.controller.js";
 import * as streamController from "../controllers/streamInfo.controller.js";
 import * as searchController from "../controllers/search.controller.js";
 import * as episodeListController from "../controllers/episodeList.controller.js";
 import * as suggestionsController from "../controllers/suggestion.controller.js";
 import * as serversController from "../controllers/servers.controller.js";
-import * as filterController from "../controllers/filter.controller.js";
 import { routeTypes } from "./category.route.js";
 
 export const createApiRoutes = (app, jsonResponse, jsonError) => {
@@ -14,14 +13,11 @@ export const createApiRoutes = (app, jsonResponse, jsonError) => {
     app.get(path, async (req, res) => {
       try {
         const data = await controllerMethod(req, res);
-        if (!res.headersSent) {
-          return jsonResponse(res, data);
-        }
+        if (!res.headersSent) return jsonResponse(res, data);
       } catch (err) {
         console.error(`Error in route ${path}:`, err);
-        if (!res.headersSent) {
+        if (!res.headersSent)
           return jsonError(res, err.message || "Internal server error");
-        }
       }
     });
   };
@@ -30,14 +26,11 @@ export const createApiRoutes = (app, jsonResponse, jsonError) => {
     app.get(route, async (req, res) => {
       try {
         const data = await homeInfoController.getHomeInfo(req, res);
-        if (!res.headersSent) {
-          return jsonResponse(res, data);
-        }
+        if (!res.headersSent) return jsonResponse(res, data);
       } catch (err) {
         console.error("Error in home route:", err);
-        if (!res.headersSent) {
+        if (!res.headersSent)
           return jsonError(res, err.message || "Internal server error");
-        }
       }
     });
   });
@@ -48,12 +41,15 @@ export const createApiRoutes = (app, jsonResponse, jsonError) => {
     )
   );
 
-  createRoute("/api/info", animeInfoController.getAnimeInfo);
+  createRoute("/api/info", mediaInfoController.getAnimeInfo);
   createRoute("/api/episodes/:id", episodeListController.getEpisodes);
   createRoute("/api/servers/:id", serversController.getServers);
-  createRoute("/api/stream", (req, res) => streamController.getStreamInfo(req, res, false));
-  createRoute("/api/stream/fallback", (req, res) => streamController.getStreamInfo(req, res, true));
+  createRoute("/api/stream", (req, res) =>
+    streamController.getStreamInfo(req, res, false)
+  );
+  createRoute("/api/stream/fallback", (req, res) =>
+    streamController.getStreamInfo(req, res, true)
+  );
   createRoute("/api/search", searchController.search);
-  createRoute("/api/filter", filterController.filter);
   createRoute("/api/search/suggest", suggestionsController.getSuggestions);
 };
