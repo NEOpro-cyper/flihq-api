e41c4a88b55e95705dc6582a11d13de39679dec4
NEOpro-cyper <kaiskdishdisdi@gmail.com>
2026-03-18 01:02:45 +0530

    Update streamInfo.controller.js

--- src/controllers/streamInfo.controller.js
+++ src/controllers/streamInfo.controller.js
@@ -1,10 +1,7 @@
 import { extractStreamingInfo } from "../extractors/streamInfo.extractor.js";
-import { extractMovieServers } from "../extractors/streamInfo.extractor.js";
 
 export const getStreamInfo = async (req, res, fallback = false) => {
   try {
-    // For movies:  /api/stream?id=12842812&server=UpCloud&type=movie
-    // For TV:      /api/stream?id=13228416&server=UpCloud&type=tv
     const id = req.query.id;
     const server = req.query.server || "upcloud";
     const type = req.query.type || "movie";
