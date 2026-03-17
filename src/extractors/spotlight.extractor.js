01f2f84387d2d9d9fea4039bf06a54770ab0556f
NEOpro-cyper <kaiskdishdisdi@gmail.com>
2026-03-17 18:15:41 +0530

    Update spotlight.extractor.js

--- src/extractors/spotlight.extractor.js
+++ src/extractors/spotlight.extractor.js
@@ -1,102 +1,46 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
-import { v1_base_url } from "../utils/base_v1.js";
+import { DEFAULT_HEADERS } from "../configs/header.config.js";
+
+const BASE_URL = "https://flixhq.tw";
 
 async function extractSpotlights() {
   try {
-    const resp = await axios.get(`https://${v1_base_url}/home`);
+    const resp = await axios.get(`${BASE_URL}/home`, { headers: DEFAULT_HEADERS });
     const $ = cheerio.load(resp.data);
 
-    const slideElements = $(
-      "div.deslide-wrap > div.container > div#slider > div.swiper-wrapper > div.swiper-slide"
-    );
+    const slides = [];
 
-    const promises = slideElements
-      .map(async (ind, ele) => {
-        const poster = $(ele)
-          .find(
-            "div.deslide-item > div.deslide-cover > div.deslide-cover-img > img.film-poster-img"
-          )
-          .attr("data-src");
-        const title = $(ele)
-          .find(
-            "div.deslide-item > div.deslide-item-content > div.desi-head-title"
-          )
-          .text()
-          .trim();
-        const japanese_title = $(ele)
-          .find(
-            "div.deslide-item > div.deslide-item-content > div.desi-head-title"
-          )
-          .attr("data-jname")
-          .trim();
-        const description = $(ele)
-          .find(
-            "div.deslide-item > div.deslide-item-content > div.desi-description"
-          )
-          .text()
-          .trim();
-        const id = $(ele)
-          .find(
-            ".deslide-item > .deslide-item-content > .desi-buttons > a:eq(0)"
-          )
-          .attr("href")
-          .split("/")
-          .pop();
-        const data_id = $(ele)
-          .find(
-            ".deslide-item > .deslide-item-content > .desi-buttons > a:eq(0)"
-          )
-          .attr("href")
-          .split("/")
-          .pop()
-          .split("-")
-          .pop();
-        const tvInfoMapping = {
-          0: "showType",
-          1: "duration",
-          2: "releaseDate",
-          3: "quality",
-          4: "episodeInfo",
-        };
+    $("#slider .swiper-wrapper .swiper-slide:not(.swiper-slide-duplicate)").each((i, ele) => {
+      const bgStyle = $(ele).attr("style") || "";
+      const bgMatch = bgStyle.match(/url\("?([^")]+)"?\)/);
+      const banner = bgMatch ? bgMatch[1] : null;
 
-        const tvInfo = {};
+      const anchor = $(ele).find("a.slide-link");
+      const href = anchor.attr("href") || "";
+      const id = href.split("/").pop();
+      const type = href.startsWith("/movie") ? "movie" : "tv";
 
-        await Promise.all(
-          $(ele)
-            .find("div.sc-detail > div.scd-item")
-            .map(async (index, element) => {
-              const key = tvInfoMapping[index];
-              let value = $(element).text().trim().replace(/\n/g, "");
+      const title = $(ele).find(".slide-caption .film-title a").text().trim();
+      const description = $(ele).find(".slide-caption .sc-desc").text().trim();
+      const quality = $(ele).find(".scd-item .quality").text().trim();
+      const duration = $(ele).find(".scd-item:contains('Duration') strong").text().trim();
+      const imdb = $(ele).find(".scd-item:contains('IMDB') strong").text().trim();
 
-              const tickContainer = $(element).find(".tick");
+      const genres = [];
+      $(ele).find(".scd-item a.slide-genre-item").each((_, g) => {
+        genres.push($(g).text().trim());
+      });
 
-              if (tickContainer.length > 0) {
-                value = {
-                  sub: tickContainer.find(".tick-sub").text().trim(),
-                  dub: tickContainer.find(".tick-dub").text().trim(),
-                };
-              }
-              tvInfo[key] = value;
-            })
-        );
-        return {
-          id,
-          data_id,
-          poster,
-          title,
-          japanese_title,
-          description,
-          tvInfo,
-        };
-      })
-      .get();
+      if (id && title) {
+        slides.push({ id, type, title, banner, description, quality, duration, imdb, genres });
+      }
+    });
 
-    const serverData = await Promise.all(promises);
-    return JSON.parse(JSON.stringify(serverData, null, 2));
+    return slides;
   } catch (error) {
-    console.error("Error fetching data:", error.message);
-    return error;
+    console.error("Error fetching spotlights:", error.message);
+    return [];
   }
 }
 
