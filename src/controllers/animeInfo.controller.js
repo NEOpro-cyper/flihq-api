9155fc45a9408a62089302df47f7dcf62c411937
NEOpro-cyper <kaiskdishdisdi@gmail.com>
2026-03-18 03:21:35 +0530

    Update animeInfo.controller.js

--- src/controllers/animeInfo.controller.js
+++ src/controllers/animeInfo.controller.js
@@ -1,27 +1,15 @@
-import extractAnimeInfo from "../extractors/animeInfo.extractor.js";
-import extractSeasons from "../extractors/seasons.extractor.js";
-import { getCachedData, setCachedData } from "../helper/cache.helper.js";
+import extractMediaInfo from "../extractors/animeInfo.extractor.js";
 
 export const getAnimeInfo = async (req, res) => {
   const { id } = req.query;
-  // const cacheKey = `animeInfo_${id}`;
-
+  // id = movie/watch-wake-up-dead-man-a-knives-out-mystery-movies-free-138514
+  // or  tv/watch-the-madison-movies-free-147781
   try {
-    // const cachedResponse = await getCachedData(cacheKey);
-    // if (cachedResponse && Object.keys(cachedResponse).length > 0) {
-    //   return cachedResponse;
-    // }
-    const [seasons, data] = await Promise.all([
-      extractSeasons(id),
-      extractAnimeInfo(id),
-    ]);
-    const responseData = { data: data, seasons: seasons };
-    // setCachedData(cacheKey, responseData).catch((err) => {
-    //   console.error("Failed to set cache:", err);
-    // });
-    return responseData;
+    if (!id) throw new Error("Missing id param");
+    const data = await extractMediaInfo(id);
+    return { data };
   } catch (e) {
     console.error(e);
-    return res.status(500).json({ error: "An error occurred" });
+    return { error: e.message };
   }
 };
