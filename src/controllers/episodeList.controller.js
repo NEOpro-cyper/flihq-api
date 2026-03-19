e4896322cf9e0c4dfcb8cae366a635c0c74b2294
NEOpro-cyper <kaiskdishdisdi@gmail.com>
2026-03-19 19:09:39 +0530

    Update episodeList.controller.js

--- src/controllers/episodeList.controller.js
+++ src/controllers/episodeList.controller.js
@@ -1,9 +1,9 @@
 import extractEpisodesList from "../extractors/episodeList.extractor.js";
 
 export const getEpisodes = async (req, res) => {
+  const { id } = req.params;
+  const type = req.query.type || null;
   try {
-    const { id } = req.params;
-    const type = req.query.type || null;
     if (!id) throw new Error("Missing id param");
     const data = await extractEpisodesList(id, type);
     return data;
